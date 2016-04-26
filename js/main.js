79aa597821816d5965fa026354bb6c32cc267477
Peter Tait <contact@petertait.com>
2016-04-26 16:50:22 +0100

    add simplified show/hide header

--- js/main.js
+++ js/main.js
@@ -18,15 +18,23 @@
   };
 
   // Navigation appear on scroll up
+  var gblHead = $('.gbl-head');
   var scrollCurrent = 0;
   $(window).scroll(function() {
     var scrollDiff = $(this).scrollTop();
     if(scrollDiff > scrollCurrent && scrollDiff > 100 && gblHeadNav.hasClass("active") === false) {
-       $('header').addClass('hide').removeClass('show');
+       $(gblHead).addClass('hide').removeClass('show');
     } else {
-       $('header').addClass('show').removeClass('hide');
+       $(gblHead).addClass('show').removeClass('hide');
     }
     scrollCurrent = scrollDiff;
+
+    if ($(this).scrollTop() >= 290) {
+      $(gblHead).addClass('gbl-head-scroll');
+    }
+    else {
+      $(gblHead).removeClass('gbl-head-scroll hide');
+    }
   });
 
   // Mobile nav
@@ -100,157 +108,6 @@
     $(window).resize(scrollPositions).scroll(servicesBG);
     scrollPositions();
     servicesBG();
-  }
-
-  // Change nav bg colour on scroll
-  var aboutHeaderContainer = $('.about .gbl-head');
-  var servicesHeaderContainer = $('.services .gbl-head');
-  var workHeaderContainer = $('.work .gbl-head');
-  var careersHeaderContainer = $('.careers .gbl-head');
-  var blogHeaderContainer = $('.blog .gbl-head');
-
-  if ($(aboutHeaderContainer).length > 0) {
-    var aboutDirectors;
-
-    var scrollPositions = debounce(function () {
-      aboutDirectors = $('#about-directors').offset().top - 200;
-    }, 100);
-
-    var headerBG = debounce(function () {
-      var windowTop = $(window).scrollTop();
-
-     if (windowTop > aboutDirectors) {
-        aboutHeaderContainer.css({'background-color':'#0ea2dc' });
-      } else {
-        aboutHeaderContainer.css({'background-color':'' });
-      }
-     }, 100);
-
-    $(window).resize(scrollPositions).scroll(headerBG);
-    scrollPositions();
-    headerBG();
-  }
-
-  if ($(servicesHeaderContainer).length > 0) {
-    var servicesStrategy;
-    var servicesHacking;
-    var servicesUX;
-    var servicesUXdesign;
-    var servicesDesign;
-    var servicesAgile;
-    var servicesTests;
-    var servicesCollab;
-
-    var scrollPositions = debounce(function () {
-      servicesStrategy = $('#services-strategy').offset().top - 200;
-      servicesHacking = $('#services-hacking').offset().top - 200;
-      servicesUX = $('#services-UX').offset().top - 200;
-      servicesUXdesign = $('#services-UX-design').offset().top - 200;
-      servicesDesign = $('#services-design').offset().top - 200;
-      servicesAgile = $('#services-agile').offset().top - 200;
-      servicesTests = $('#services-tests').offset().top - 200;
-      servicesCollab = $('#services-collab').offset().top - 200;
-    }, 100);
-
-    var headerBG = debounce(function () {
-      var windowTop = $(window).scrollTop();
-
-      if (windowTop > servicesCollab) {
-        servicesHeaderContainer.css({'background-color':'#ed4f7e' });
-      } else if (windowTop > servicesTests) {
-        servicesHeaderContainer.css({'background-color':'#9b5ca4' });
-      } else if (windowTop > servicesAgile) {
-        servicesHeaderContainer.css({'background-color':'#faad40' });
-      } else if (windowTop > servicesDesign) {
-        servicesHeaderContainer.css({'background-color':'#a4ce4e' });
-      } else if (windowTop > servicesUXdesign) {
-        servicesHeaderContainer.css({'background-color':'#37bec0' });
-      } else if (windowTop > servicesUX) {
-        servicesHeaderContainer.css({'background-color':'#0ea2dc' });
-      } else if (windowTop > servicesHacking) {
-        servicesHeaderContainer.css({'background-color':'#ed4f7e' });
-      } else if (windowTop > servicesStrategy) {
-        servicesHeaderContainer.css({'background-color':'#9b5ca4' });
-      } else {
-        servicesHeaderContainer.css({'background-color':'' });
-      }
-    }, 100);
-
-    $(window).resize(scrollPositions).scroll(headerBG);
-    scrollPositions();
-    headerBG();
-  }
-
-  if ($(workHeaderContainer).length > 0) {
-    var workPharmaceutical;
-    var workIntranets;
-    var workSelfDirected;
-    var workNextGen;
-    var workEnterpriseMobile;
-    var workRailTravel;
-    var workHighFrequency;
-
-    var scrollPositions = debounce(function () {
-      workPharmaceutical = $('#work-pharmaceutical').offset().top - 200;
-      workIntranets = $('#work-intranets').offset().top - 200;
-      workSelfDirected = $('#work-self-directed').offset().top - 200;
-      workNextGen = $('#work-next-gen').offset().top - 200;
-      workEnterpriseMobile = $('#work-enterprise-mobile').offset().top - 200;
-      workRailTravel = $('#work-rail-travel').offset().top - 200;
-      workHighFrequency = $('#work-high-frequency').offset().top - 200;
-    }, 100);
-
-    var headerBG = debounce(function () {
-      var windowTop = $(window).scrollTop();
-
-     if (windowTop > workHighFrequency) {
-        workHeaderContainer.css({'background-color':'#ed4f7e' });
-      } else if (windowTop > workRailTravel) {
-        workHeaderContainer.css({'background-color':'#faad40' });
-      } else if (windowTop > workEnterpriseMobile) {
-        workHeaderContainer.css({'background-color':'#a4ce4e' });
-      } else if (windowTop > workNextGen) {
-        workHeaderContainer.css({'background-color':'#0ea2dc' });
-      } else if (windowTop > workSelfDirected) {
-        workHeaderContainer.css({'background-color':'#37bec0' });
-      } else if (windowTop > workIntranets) {
-        workHeaderContainer.css({'background-color':'#ed4f7e' });
-      } else if (windowTop > workPharmaceutical) {
-        workHeaderContainer.css({'background-color':'#9b5ca4' });
-      } else {
-        workHeaderContainer.css({'background-color':'' });
-      }
-     }, 100);
-
-    $(window).resize(scrollPositions).scroll(headerBG);
-    scrollPositions();
-    headerBG();
-  }
-
-  if ($(careersHeaderContainer).length > 0) {
-    var careersBenefits;
-    var careersKaizen;
-
-    var scrollPositions = debounce(function () {
-      careersBenefits = $('#careers-benefits').offset().top - 200;
-      careersKaizen = $('#careers-kaizen').offset().top - 200;
-    }, 100);
-
-    var headerBG = debounce(function () {
-      var windowTop = $(window).scrollTop();
-
-     if (windowTop > careersBenefits) {
-        careersHeaderContainer.css({'background-color':'#0ea2dc' });
-      } else if (windowTop > careersKaizen) {
-        careersHeaderContainer.css({'background-color':'#37bec0' });
-      } else {
-        careersHeaderContainer.css({'background-color':'' });
-      }
-     }, 100);
-
-    $(window).resize(scrollPositions).scroll(headerBG);
-    scrollPositions();
-    headerBG();
   }
 
   // Smooth Scrolling for Internal Links
